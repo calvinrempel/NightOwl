924aa76f66a4a84ddb5e254eeda8f8db8ad23528
Nav Bhatti <bhatti.nav@gmail.com>
2015-04-28 00:13:43 -0700

    Added UI skeleton

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,22 @@
 	var  app = angular.module('NightOwl', []);
 
 	app.controller('MainController', function($scope, $http) {
-		$scope.msg = "IT WORKS!";
+		$scope.selected = 'login';
 	});
+
+	app.controller('LoginController', function($scope, $http) {
+		$scope.msg = "SHOW LOGIN"
+	});
+
+	app.controller('ListController', function($scope, $http) {
+		$scope.region = "west";
+		$scope.msg = "LIST THE CODES";
+	});
+
+	app.controller('AuditController', function($scope, $http) {
+		$scope.msg = "LIST THE AUDITS";
+	});
+
 
 
 })();
