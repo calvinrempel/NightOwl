5d840f1ad6767468c22e431b3db6fd08057397b1
Nav Bhatti <bhatti.nav@gmail.com>
2015-04-28 11:06:11 -0700

    implemented basic launch code listing

--- public/js/app.js
+++ public/js/app.js
@@ -2,23 +2,32 @@
 	var  app = angular.module('NightOwl', []);
 
 	app.controller('MainController', function($scope, $http) {
+
 		$scope.selected = 'login';
+		$.getJSON('js/config.json', function(json, textStatus) {
+				$scope.config = json;
+		});
 	});
 
 	app.controller('LoginController', function($scope, $http) {
-		$scope.msg = "SHOW LOGIN"
+		$scope.msg = "SHOW LOGIN";
 	});
 
 	app.controller('ListController', function($scope, $http) {
 		$scope.region = "west";
-		$scope.msg = "LIST THE CODES";
+		$.getJSON('js/codes.json', function(json, textStatus) {
+			$scope.trees = json;
+		});
+
+		$scope.echo = function(){
+			console.log("HELLO");
+			console.log($scope.trees);
+		};
 	});
 
 	app.controller('AuditController', function($scope, $http) {
 		$scope.msg = "LIST THE AUDITS";
 	});
-
-
 
 })();
 
