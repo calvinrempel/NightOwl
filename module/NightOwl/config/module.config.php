072e2b3aa4386c7ce0bf1fd87ab617bd6f61eea9
Marc Vouve <mvouve@gmail.com>
2015-04-30 18:53:19 -0700

    multiple values added.

--- module/NightOwl/config/module.config.php
+++ module/NightOwl/config/module.config.php
@@ -14,9 +14,10 @@ return array(
              'login' => array(
                  'type'    => 'segment',
                  'options' => array(
-                     'route'    => '/login[/:id]',
+                     'route'    => '/login[/:id][/:pw]',
                      'constraints' => array(
                          'id' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                         'pw' => '[a-zA-Z][a-zA-Z0-9_-]*',
                      ),
                      'defaults' => array(
                          'controller' => 'NightOwl\Controller\AuthRest',
