1243338c26c1792c2664de42da45c1d9e8489fbd
Nav Bhatti <bhatti.nav@gmail.com>
2015-04-30 22:05:21 -0700

    Implemented drop down list for trees and filtering

--- public/js/app.js
+++ public/js/app.js
@@ -2,25 +2,48 @@
 	var  app = angular.module('NightOwl', []);
 
 	app.controller('MainController', function($scope, $http) {
-
+		// Start on the login page
 		$scope.selected = 'login';
-		$.getJSON('js/config.json', function(json, textStatus) {
-			$scope.config = json;
-			console.log($scope.config.trees);
+
+		// Load configuration data
+		loadConfig(function(data){
+			$scope.config = data;
 		});
+
 	});
 
-	app.controller('LoginController', function($scope, $http, $compile) {
+	app.controller('LoginController', function($scope, $http) {
 		//ADD LOGIN FUNCTIONALITY HERE
 	});
 
 	app.controller('ListController', function($scope, $http) {
-		$scope.depth = 0;
-		$.getJSON('js/codes.json', function(json, textStatus) {
-			$scope.launchCodes = json;
+		// Default region is west
+		$scope.region = "west";
+
+		// Load the launch codes
+		loadCodes(function(data){
+			$scope.launchCodes = data;
 		});
 
-		$scope.region = "west";
+		$scope.filterResults = function(){
+			loadCodes(function(data){
+				$scope.launchCodes = data;
+				
+				var filter = 	
+					( $scope.tree ? $scope.tree.name : "" ) + "/" +
+					( $scope.subtree ? $scope.subtree : "" );
+
+				for( tree in $scope.launchCodes ){
+					if(tree.indexOf(filter) == -1){
+						delete $scope.launchCodes[tree];
+						console.log($scope.launchCodes);
+					}
+				}
+
+			});
+
+
+		};
 
 	});
 
@@ -29,4 +52,22 @@
 	});
 
 })();
+
+function loadConfig(_callback){
+	var url = makeURL("config");
+	$.getJSON(url, function(json, textStatus) {
+		_callback(json);
+	});
+}
+
+function loadCodes(_callback){
+	var url = makeURL("codes");
+	$.getJSON(url, function(json, textStatus) {
+		_callback(json);
+	});
+}
+
+function makeURL(string){
+	return "js/" + string + ".json";
+}
 
