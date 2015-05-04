4346392f6bf52e2443d454e639294c80d367b011
Calvin Rempel <rempel.calvin@gmail.com>
2015-05-03 22:32:22 -0700

    Added ability to edit owner field in metadata. Defaults to current logged in user if not set
    Signed-off-by: Calvin Rempel <rempel.calvin@gmail.com>

--- module/NightOwl/src/NightOwl/Model/Auth.php
+++ module/NightOwl/src/NightOwl/Model/Auth.php
@@ -75,6 +75,29 @@ class Auth {
     }
 
     /**
+	 * Get the username of the user with the given authentication token.
+	 *
+	 * Params:
+	 *		$token   : the users authentication token
+	 *
+	 * Returns: The logged in users username if the token is value, False if the
+     *          token is invalid.
+	 *
+	 * Author: Calvin Rempel
+	 * Date: May 3, 2015
+	 */
+    public function getCurrentUser($token)
+    {
+        $utoken = array('key' => $token);
+        if(($user = $this->db->Auth->findOne($utoken)) === NULL )
+        {
+            return false;
+        }
+
+        return $user['user'];
+    }
+
+    /**
      * I don't understand how I'm supposed to get this any other way.
      */
     private function getConfig()
