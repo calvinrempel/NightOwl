edeb11c3e782e686d20c04edb1eec7d30923b282
Nav Bhatti <bhatti.nav@gmail.com>
2015-05-15 19:06:34 -0700

    added automatic audit loading

--- public/app/controllers/audit.js
+++ public/app/controllers/audit.js
@@ -1,28 +1,11 @@
 
 (function(){
 app.controller('AuditController', function($scope, $http, audits) {
-	$scope.auditFilters = {
-        filterBy : $scope.config.auditFilters[0],
-        filter : ''
-    };
+	
 
     $scope.filterAudits = function(){
-        loadAudits();
+        $scope.loadAudits();
     };
-
-    function loadAudits(){
-        audits.load($scope.auditFilters, function(success, data){
-            if(success){
-                $scope.auditList = data;
-            } else if( data === 401 ){
-				if ($scope.selected !== 'login')
-					location.reload();
-			}
-        });
-    }
-
-    loadAudits();
-
 
 });
 })();
--- public/app/controllers/list.js
+++ public/app/controllers/list.js
@@ -1,21 +1,5 @@
 (function(){
-app.controller('ListController', function($scope, $http, codes, auth, loading) {
-
-		
-
-		function init(){
-			$scope.createMode = false;
-			$scope.editMode = [];
-			$scope.newCode = {};
-			$scope.sort = {};
-			$scope.prefixes = [];
-			$scope.dc = {};
-			$scope.filters = {};
-			$scope.sortOptions = [];
-			$scope.setDataCenter($scope.config.dataCenters[0]);
-		}
-
-
+app.controller('ListController', function($scope) {
 		
 
         // EDIT MODE FUNCTIONS
@@ -30,20 +14,6 @@ app.controller('ListController', function($scope, $http, codes, auth, loading) {
 		}
 
 		// CODE CRUD FUNCTIONS
-        $scope.loadCodes = function(){
-            loading.start();
-            codes.load($scope.filters)
-            .success(function(data){
-                listCodes( data.codes );
-            })
-            .error(function(data, status){
-                if(status == 401 && $scope.selected !== "login")
-                    location.reload();
-            })
-            .finally(function(){
-                loading.stop()
-            });
-        }
 
 		$scope.saveCode = function(code){
             loading.start();
@@ -79,17 +49,7 @@ app.controller('ListController', function($scope, $http, codes, auth, loading) {
             }
 		}
 
-        $scope.setDataCenter = function(dataCenter){
-            $scope.dc = dataCenter;
-            $scope.prefixes = buildList(dataCenter.prefixes);
-            $scope.filters = {
-                dataCenter : dataCenter.value,
-                filterBy : $scope.config.filters[0],
-                prefix: $scope.prefixes[0],
-                filter : ''
-            };
-            $scope.loadCodes();
-        }
+        
 
 
 
@@ -106,45 +66,7 @@ app.controller('ListController', function($scope, $http, codes, auth, loading) {
 			}
 		}
 
-        $scope.resetFilters = function(){
-            if($scope.dc !== undefined)
-                $scope.setDataCenter( $scope.dc );
-            else
-                $scope.setDataCenter( $scope.config.dataCenters[0] );
-        }
-
-        function buildList(object, branch){
-            var array = [];
-            for(key in object){
-                if(!branch){
-                    array.push(key)
-                    if(!$.isEmptyObject(object[key]))
-                        array = array.concat(buildList(object[key], key));
-                }else{
-                    array.push(branch + "/" + key);
-                    if(!$.isEmptyObject(object[key]))
-                        array = array.concat(buildList(object[key], branch + "/" + key));
-                }
-
-            }
-            return array;
-        }
-
-        function trimKeys( codes ){
-            for (var i = 0; i < codes.length; i++) {
-                codes[i].key = codes[i].key.replace($scope.filters.prefix + "/", "");
-            }
-            return codes;
-        }
-
-        function listCodes( codes ){
-            $scope.selectTab("list");
-            $scope.launchCodes = trimKeys( codes );
-            $scope.sortOptions = Object.keys( codes );
-            $scope.sort.field = $scope.sortOptions[0];
-            $scope.sort.desc = true;
-        }
-
+        
 
 	});
 
